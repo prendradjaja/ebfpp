f219f54846dd1914f0ac3cf5344f138f9a349406
johnvantran6592 <johnvantran6592@gmail.com>
2014-12-07 21:18:59 -0800

    Refactored the big switch to use fn calls

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -19,6 +19,10 @@ function initSession(code)
         "pc": 0,
         "pc_mark": new Array()
     }
+
+    tokens = session.tokens
+    memory = session.memory
+    pc_mark = session.pc_mark
 }
 
 /**
@@ -29,77 +33,11 @@ function initSession(code)
 function interpret(step)
 {
     isRunning(true);
-    var tokens = session.tokens
-    var memory = session.memory
-    var pc_mark = session.pc_mark
-
     while(true) {
         var inst = tokens[session.pc]
         switch (inst.type) {
             case 'bf_command':
-                switch(inst.cmd) {
-                    case '+':
-                        var old = memory[session.pointer]
-                        var newVal = old == 255 ? 0 : old + 1;
-                        memory[session.pointer] = newVal
-                        session.pc++;
-                        break;
-                    case '-':
-                        var old = memory[session.pointer];
-                        var newVal = old == 0 ? 255 : old-1
-                        memory[session.pointer] = newVal
-                        session.pc++;
-                        break;
-                    case '<':
-                        var old = session.pointer;
-                        var newPtr;
-
-                        if (old == 0) {
-                            newPtr = 255;
-                        } else {
-                            newPtr = old - 1;
-                        }
-
-                        session.pointer = newPtr;
-                        session.pc++;
-                        break;
-                    case '>':
-                        var old = session.pointer;
-                        var newPtr;
-
-                        if (old == 255) {
-                            newPtr = 0;
-                        } else {
-                            newPtr = old + 1;
-                        }
-
-                        session.pointer = newPtr;
-                        session.pc++;
-                        break;
-                    case ',':
-                        var content = prompt("Enter a single character input")
-                        var c = content[0]
-                        memory[session.pointer] = c.charCodeAt(0)
-                        session.pc++;
-                        break;
-                    case '.':
-                        writeToOutput(
-                            String.fromCharCode(memory[session.pointer]));
-                        session.pc++;
-                        break;
-                    case ']':
-                        new_pc = pc_mark.pop()
-                        if(memory[session.pointer] > 0) {
-                            session.pc = new_pc;
-                            break;
-                        }
-                        session.pc++;
-                        break;
-                    case '[':
-                        pc_mark.push(session.pc);
-                        session.pc++;
-                        break;
-                }
+                interpret_bf_command(inst);
                 break;
             case '#':
                 sigBreak(session);
@@ -120,5 +58,71 @@ function interpret(step)
             }
             break;
         }
+    }
+}
+
+function interpret_bf_command(inst) {
+    switch(inst.cmd) {
+        case '+':
+            var old = memory[session.pointer]
+            var newVal = old == 255 ? 0 : old + 1;
+            memory[session.pointer] = newVal
+            session.pc++;
+            break;
+        case '-':
+            var old = memory[session.pointer];
+            var newVal = old == 0 ? 255 : old-1
+            memory[session.pointer] = newVal
+            session.pc++;
+            break;
+        case '<':
+            var old = session.pointer;
+            var newPtr;
+
+            if (old == 0) {
+                newPtr = 255;
+            } else {
+                newPtr = old - 1;
+            }
+
+            session.pointer = newPtr;
+            session.pc++;
+            break;
+        case '>':
+            var old = session.pointer;
+            var newPtr;
+
+            if (old == 255) {
+                newPtr = 0;
+            } else {
+                newPtr = old + 1;
+            }
+
+            session.pointer = newPtr;
+            session.pc++;
+            break;
+        case ',':
+            var content = prompt("Enter a single character input")
+            var c = content[0]
+            memory[session.pointer] = c.charCodeAt(0)
+            session.pc++;
+            break;
+        case '.':
+            writeToOutput(
+                String.fromCharCode(memory[session.pointer]));
+            session.pc++;
+            break;
+        case ']':
+            new_pc = pc_mark.pop()
+            if(memory[session.pointer] > 0) {
+                session.pc = new_pc;
+                break;
+            }
+            session.pc++;
+            break;
+        case '[':
+            pc_mark.push(session.pc);
+            session.pc++;
+            break;
     }
 }
